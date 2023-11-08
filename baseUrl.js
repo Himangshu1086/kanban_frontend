9dac8868bc3b67612f86123f9c41db9596c14951
Himangshu1086 <baishyahimangshu499@gmail.com>
2023-11-08 14:59:02 +0530

    update

--- baseUrl.js
+++ baseUrl.js
@@ -1,5 +1,5 @@
 
 
-const baseUrl = process.env.NODE_ENV === 'production' ? "" :'http://localhost:5000'
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://kanban-dashboard-five.vercel.app" :'http://localhost:5000'
 
 export default baseUrl;
